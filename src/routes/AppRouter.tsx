1ea69ab66e92fdc7f595e7c701a0011357763695
Abdullah <abdulhhany69@gmail.com>
2025-10-27 10:13:32 +0300

    add: error page

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -8,11 +8,13 @@ import Products from "@pages/Products";
 import AboutUs from "@pages/AboutUs";
 import Login from "@pages/Login";
 import Register from "@pages/Register";
+import Error from "@pages/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
