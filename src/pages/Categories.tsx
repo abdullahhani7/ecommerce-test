60a38df38ddf1f68264f31cb1475cf6f9e5e3b1c
Abdullah <abdulhhany69@gmail.com>
2025-10-28 10:26:49 +0300

    useAppDispatch & useAppSelector

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actGetCategories } from "@store/categories/categoriesSlice";
+
 import { Container, Row, Col } from "react-bootstrap";
 import { Category } from "@components/eCommerce";
 
 const Categories = () => {
+  const dispatch = useAppDispatch();
+  const { records, loading, error } = useAppSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    dispatch(actGetCategories());
+  }, [dispatch]);
+
   return (
     <Container>
       <Row>
