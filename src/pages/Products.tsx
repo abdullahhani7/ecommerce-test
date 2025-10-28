5fde29a84fb47d7139049351ccaeee11b9026c2b
Abdullah <abdulhhany69@gmail.com>
2025-10-28 11:38:00 +0300

    Products Slice, Thunk and Rendering

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,24 +1,38 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { actGetProductsByCatPrefix } from "@store/products/productsSlice";
+import { useParams } from "react-router-dom";
+
 import { Container, Row, Col } from "react-bootstrap";
 import { Product } from "@components/eCommerce";
 const Products = () => {
+  const dispatch = useAppDispatch();
+  const { records, loading, error } = useAppSelector((state) => state.products);
+
+  const { prefix } = useParams();
+
+  useEffect(() => {
+    dispatch(actGetProductsByCatPrefix(prefix as string));
+  }, [dispatch, prefix]);
+
+  const productsList =
+    records.length > 0
+      ? records.map((record) => (
+          <Col
+            key={record.id}
+            xs={6}
+            md={3}
+            className="d-flex justify-content-center mb-5 mt-2"
+          >
+            <Product {...record} />
+          </Col>
+        ))
+      : "There are no categories";
+
   return (
     <Container>
       <Row>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
-        <Col xs={6} md={3} className="d-flex justify-content-center mb-5 mt-2">
-          <Product />
-        </Col>
+         {productsList}
       </Row>
     </Container>
   );
---
+++ src/store/products/productsSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import actGetProductsByCatPrefix from "./act/actGetProductsByCatPrefix";
+import type { TProduct } from "@customTypes/product";
+import type { TLoading } from "@customTypes/shared";
+interface ICategoriesState {
+  records: TProduct[];
+  loading: TLoading;
+  error: null | string;
+}
+
+const initialState: ICategoriesState = {
+  records: [],
+  loading: "idle",
+  error: null,
+};
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(actGetProductsByCatPrefix.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+    builder.addCase(actGetProductsByCatPrefix.fulfilled, (state, action) => {
+      state.loading = "succeeded";
+      state.records = action.payload;
+    });
+    builder.addCase(actGetProductsByCatPrefix.rejected, (state, action) => {
+      state.loading = "failed";
+      if (action.payload && typeof action.payload === "string") {
+        state.error = action.payload;
+      }
+    });
+  },
+});
+
+export default categoriesSlice.reducer;
+export { actGetProductsByCatPrefix };
