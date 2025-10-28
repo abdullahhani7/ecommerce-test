3d1043024a2151908fcec0ca222f9b9992fc947d
Abdullah <abdulhhany69@gmail.com>
2025-10-28 09:55:21 +0300

    Promise Lifecycle Actions

--- src/store/categories/act/actGetCategories.ts
+++ src/store/categories/act/actGetCategories.ts
@@ -11,7 +11,7 @@ const actGetCategories = createAsyncThunk(
       const response = await axios.get<TResponse>(
         "http://localhost:5000/categories"
       );
-      return response;
+      return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
         return rejectWithValue(error.response?.data.message || error.message);
