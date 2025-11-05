40d51c389d5f1417fee5160ac53342e381bee621
Abdullah <abdulhhany69@gmail.com>
2025-11-05 13:16:47 +0200

    Persist state with Redux Persist using Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import AppRouter from "@routes/AppRouter";
 // redux
 import { Provider } from "react-redux";
-import store from "@store/index";
+import store, { persistor } from "@store/index";
+import { PersistGate } from "redux-persist/integration/react";
 // styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import categories from "./categories/categoriesSlice";
 import products from "./products/productsSlice";
 import cart from "./cart/cartSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({ categories, products, cart });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { categories, products, cart },
+  reducer: persistedReducer,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
@@ -12,4 +23,5 @@ export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
